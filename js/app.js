3dac9112ff84b321ef9d994920eac123370c1a54
James McDaniel <icathaid@gmail.com>
2018-02-06 16:40:57 -0800

    commit message

---
+++ js/app.js
@@ -0,0 +1,61 @@
+//alert ('things');
+var numRight = 0
+var answer1 = prompt('Yes or No?').toLowerCase().trim();
+    console.log(answer1);
+    if (answer1 === 'yes'){
+        alert('Yes');
+        numRight +=1;
+    } else if (answer1 ==='no'){
+        alert('No');
+    } else {
+        alert('One or the other');
+    }
+    console.log('Number Correct: ' + numRight);
+
+var answer2 = prompt('Yes or No 2?').toLowerCase().trim();
+    console.log(answer2);
+    if (answer2 === 'yes'){
+        alert('Yes');
+        numRight +=1;
+    } else if (answer2 === 'no'){
+        alert('No');
+    } else {
+        alert('One or the other');
+    }
+    console.log('Number Correct: ' + numRight);
+
+var answer3 = prompt('Yes or No 3?').toLowerCase().trim();
+    console.log(answer3);
+    if (answer3 === 'yes'){
+        alert('Yes');
+        numRight +=1;
+    } else if (answer3 === 'no'){
+        alert('No');
+    } else {
+        alert('One or the other');
+    }
+    console.log('Number Correct: ' + numRight);
+
+var answer4 = prompt('Yes or No 4?').toLowerCase().trim();
+    console.log(answer4);
+    if (answer4 === 'yes'){
+        alert('Yes');
+        numRight +=1;
+    } else if (answer4 === 'no'){
+        alert('No');
+    } else {
+        alert('One or the other');
+    }
+    console.log('Number Correct: ' + numRight);
+
+var answer5 = prompt('Yes or No 5?').toLowerCase().trim();
+    console.log(answer5);
+    if (answer5 === 'yes'){
+        alert('Yes');
+        numRight +=1;
+    } else if (answer5 === 'no'){
+        alert('No');
+    } else {
+        alert('One or the other');
+    }
+    console.log('Number Correct: ' + numRight);
